eebc4ecc34b54d8b720ce44a7d851bf6fb73b7b3
RogerDewald <daniel.r.dewald@gmail.com>
2023-10-16 17:30:13 -0500

    colors change

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -7,15 +7,24 @@
 
 require("catppuccin").setup({
 	favlour = "mocha",
-	no_italic = false,
+	no_italic = true,
 })
 
 
 function ColorMyPencils(color) 
 	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
-
+    vim.api.nvim_set_hl(0, "Normal", {bg="none"})
+    vim.api.nvim_set_hl(0, "NormalFloat", {bg="none"})
+    
 
 end
 
+
+function DarkenMyPencils(color) 
+	color = color or "catppuccin"
+	vim.cmd.colorscheme(color)
+    vim.api.nvim_set_hl(0, "Normal", {})
+    vim.api.nvim_set_hl(0, "NormalFloat", {})
+end
 ColorMyPencils()
